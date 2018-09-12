6c933c25bf19b42a8395ecff3b284b2a047e3c27
Zitha-Dumisani <42849208+Zitha-Dumisani@users.noreply.github.com>
2018-09-12 15:44:03 +0200

    Add files via upload
    Changing thread class to use Recursive Task. Photosynthesis class reads data but still needs to be completed.

--- Photosynthesis.java
+++ Photosynthesis.java
@@ -1,5 +1,125 @@
+//Photosynthesis program
+
+//imports
+import java.io.*;
+import java.util.concurrent.ForkJoinPool;
+
 public class Photosynthesis{
+	//       photosynthesis
+	
+	public static String Tree_Averages(double[][] rTerrain,int[][] treeData,int NoTrees){
+		//
+		thread t=new thread(rTerrain,treeData,0,NoTrees-1);
+		return ForkJoinPool.commonPool().invoke(t);
+	
+	}
     public static void main(String[] args) {
+        try{
+            //FileReader fr=new FileReader("sample_input.txt"); 
+            FileReader fr=new FileReader("1.txt");   
         
+        BufferedReader br=new BufferedReader(fr);
+        
+        
+        int[] size=new int[2];
+        String sLine;    
+        int ibuffer=0;
+        int iTrees=0;
+        int[][] LTrees;
+        boolean b=true;
+
+        // read input =================================================================================
+        //map size
+        sLine=br.readLine();
+     
+    
+        String[] s=new String[2];
+        s=sLine.split(" ");
+        size[0]=Integer.parseInt(s[0]);
+        size[1]=Integer.parseInt(s[1]);
+
+        System.out.println("map size saved");
+
+        //save terrain
+        sLine=br.readLine();
+        
+        String[] sAve=sLine.split(" ");
+       
+        double[][] rAve=new double[size[0]][size[1]]; 
+        for (int i=0;i<size[0]*size[1];i++){
+          rAve[i / size[1] ][i % size[1]]=Double.parseDouble(sAve[i]);
+        }
+
+        System.out.println("terrain saved");
+        
+
+        //number of trees
+        sLine=br.readLine();
+        iTrees=Integer.parseInt(sLine);
+       
+
+       
+        //tree positions and size
+        LTrees=new int[iTrees][3];
+        for (int i=0;i<iTrees;i++){
+            sLine=br.readLine();
+            String[] s1=sLine.split(" ");
+            LTrees[i][0]=Integer.parseInt(s1[0]);
+            LTrees[i][1]=Integer.parseInt(s1[1]);
+            LTrees[i][2]=Integer.parseInt(s1[2]);
+           
+        }  
+        System.out.println("trees data saved saved. \n\nComputing...");
+        //System.out.println(LTrees[iTrees-1][0]);
+       
+        br.close();    
+        fr.close(); 
+
+     
+        //==============================================================================================
+        
+        double[] TAve=new double[iTrees];
+        double sumTot=0;
+        //calculate trees averages  ***************************
+		
+		//fix data
+		
+		//
+		System.out.println("aaaaaaaaaaaa");
+		System.out.println("_"+(iTrees));
+		System.out.println(Tree_Averages(rAve,LTrees,iTrees));
+
+		
+		/*
+        for (int i=0;i<iTrees;i++){
+            double sum=0;
+            //sum pieces of tree
+            for (int j1=0;j1<LTrees[i][2];j1++){
+                for (int j2=0;j2<LTrees[i][2];j2++){
+                    if (j1+LTrees[i][0]<size[0] && j2+LTrees[i][1]<size[1] ){
+                      sum=sum+rAve[j1+LTrees[i][0]][j2+LTrees[i][1]]; 
+                      
+                    }             
+                }
+            }
+            TAve[i]=sum;
+            sumTot=sum+sumTot;
+        }
+        
+        
+        //print out
+        System.out.println(sumTot/iTrees);
+        System.out.println(iTrees);
+        
+        for (int i=0;i<iTrees;i++){
+            System.out.println(TAve[i]);
+        }
+
+        */
+
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+
+        }
     }
 }
